854f20a87d309f31349aac45673d938d6c777ac1
Dan Hixon <danhixon@gmail.com>
2019-03-13 12:21:46 -0700

    protect from CVE-2019-5419
    https://groups.google.com/forum/#!topic/rubyonrails-security/GN7w9fFAQeI

--- app/controllers/brochure/home_controller.rb
+++ app/controllers/brochure/home_controller.rb
@@ -1,7 +1,9 @@
 module Brochure
 	class HomeController < ApplicationController
 	  def four_zero_four
-	    render 'four_zero_four', :status=>404
+	    respond_to |format| 
+	      format.html { render 'four_zero_four', :status=>404 } 
+	    end
 	  end
 	end
 end
